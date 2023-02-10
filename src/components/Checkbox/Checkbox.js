e551d761dbea58b5221c7a3441c82bbb8a3d6490
Venkat.R <ramsunvtech@gmail.com>
2023-02-10 14:44:06 +0800

    tech(responsive-ui-table): updates

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -4,11 +4,11 @@ import './checkbox.css';
 const noop = () => {}
 
 export default function Checkbox(props) {
-  const { label, name, onSelect = noop } = props;
+  const { label, name, onSelect = noop, checked } = props;
 
   return (
     <label className="pure-material-checkbox">
-      <input type="checkbox" name={name} onChange={e => onSelect(e)} />
+      <input type="checkbox" name={name} onChange={e => onSelect(e)} checked={checked} />
       <span></span>
     </label>
   )
--- src/components/RadioButton/RadioButton.js
+++ src/components/RadioButton/RadioButton.js
@@ -3,11 +3,11 @@ import './radio.css';
 
 const noop = () => {}
 export default function RadioButton(props) {
-  const { name, onSelect = noop } = props;
+  const { name, onSelect = noop, checked } = props;
 
   return (
     <label className="pure-material-radio">
-      <input type="radio" name={name} onChange={e => onSelect(e)} />
+      <input type="radio" name={name} onChange={e => onSelect(e)} checked={checked} />
       <span></span>
     </label>
   )
